e9be80a01945a5380093202bea8328c2954f5f90
stanley-roache <stan.roache@gmail.com>
2018-07-02 13:58:05 +1200

    started cannibalising the 3d globe into a globe country selector, added component, css file and linked this to index.html

---
+++ client/components/GlobeSelector.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+import * as d3 from 'd3'
+
+export default class GlobeSelector extends Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <div> nu'in </div>
+        )
+    }
+} 
