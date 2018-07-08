1bbff93cc0da644d6183fb85ad75b3be9a4e8ef8
stanley-roache <stan.roache@gmail.com>
2018-07-08 14:36:08 +1200

    mostly refactored code with variables moved to state and promises to handle function order, not working yet

--- client/components/GlobeSelector.jsx
+++ client/components/GlobeSelector.jsx
@@ -16,20 +16,45 @@ const topojson = require('topojson')
 class GlobeSelector extends Component {
     constructor(props) {
         super(props)
+
+        this.state = {
+          falseDOM: null,
+          focused: null,
+          projection: null,
+          path: null,
+          svg: null,
+          countryToolTip: null,
+          countryList: null,
+          countryData: null,
+          countries: null,
+          world: null
+        }
+
+        this.initialiseGlobe = this.initialiseGlobe.bind(this)
+        this.drawCountriesAndSetListeners = this.drawCountriesAndSetListeners.bind(this)
     }
 
     componentDidMount() {
-        this.createGlobe()
+        this.initialiseGlobe()
+          .then(() => this.readFiles())
+          .then(([world, countries]) => {
+            return this.drawCountriesAndSetListeners(world, countries)
+          })
+          .then(() => {
+            this.attachGlobeSelectListener()
+          })
+          .catch(err => {
+            throw err
+          })
     }
 
-    createGlobe() {
-        const faux = this.props.connectFauxDOM('div', 'globeContainer')
+    initialiseGlobe() {
+        const falseDOM = this.props.connectFauxDOM('div', 'globeContainer')
         const props = this.props
 
         let width = 600,
             height = 500,
-            sens = 0.25,
-            focused;
+            sens = 0.25;
 
         let projection = d3.geoOrthographic()
             .scale(245)
@@ -40,7 +65,7 @@ class GlobeSelector extends Component {
         let path = d3.geoPath()
             .projection(projection)
 
-        let svg = d3.select(faux).append('svg')
+        let svg = d3.select(falseDOM).append('svg')
             .attr('width', width)
             .attr('height', height)
 
@@ -49,101 +74,145 @@ class GlobeSelector extends Component {
             .attr('class', 'water')
             .attr('d', path)
 
-        let countryToolTip = d3.select(faux).append('div')
+        let countryToolTip = d3.select(falseDOM).append('div')
             .attr('class', 'countryToolTip')
 
-        let countryList = d3.select(faux).append('select')
+        let countryList = d3.select(falseDOM).append('select')
             .attr('class', 'globe-select')
             .attr('name', 'countries')
 
-
-        let readInData = Promise.all([
-            d3.json('/world-110m.json'),
-            d3.tsv('/world-110m-country-names.tsv')
-        ])
-
-        readInData.then(([world, countries]) => {
-                ready(world, countries)
-            })
-            .catch(err => {
-                throw err
-            })
-
-        function ready(worldJson, countryData, country) {
-            let countries = topojson.feature(worldJson, worldJson.objects.countries).features
-
-            const countryById = {}
-
-            // pushing countries into droplist
-            countryData.forEach(d => {
-                countryById[d.id] = d.name 
-                countryList.append('option')
-                        .text(d.name)
-                        .property('value', d.id)
-            })
-
-            // drawing countries on globe
-            let world = svg.selectAll('path.land')
-                .data(countries)
-                .enter().append('path')
-                .attr('class', 'land')
-                .attr('d', path)
-
-            // hover on country info
-            world.on("mouseover", d => {
-                countryToolTip.text(countryById[d.id])
-                .style("left", (d3.event.pageX + 7) + "px")
-                .style("top", (d3.event.pageY - 15) + "px")
-                .style("display", "block")
-                .style("opacity", 1);
-                props.drawFauxDOM()
-              })
-              .on("mouseout", d => {
-                countryToolTip.style("opacity", 0)
-                .style("display", "none");
-                props.drawFauxDOM()
-              })
-              .on("mousemove", d => {
-                countryToolTip.style("left", (d3.event.pageX + 7) + "px")
-                .style("top", (d3.event.pageY - 15) + "px");
-                props.drawFauxDOM()
-              });
-
-            d3.select(faux)
-                .select("select")
-                .on("change", function (e) {
-                    const selectedName = d3.select(faux).select('.globe-select').node().component.value
-
-                    props.dispatch(selectCountryAction(selectedName))
-
-                    const selectedId = countryData.find(e => e.name == selectedName).id
-
-                    
-                    let rotate = projection.rotate(),
-                        focusedCountry = countries.find(e => Number(e.id) === Number(selectedId)),
-                        p = d3.geoCentroid(focusedCountry);
-
-                    svg.selectAll(".focused").classed("focused", focused = false);
-            
-                    //Globe rotating
-                    d3.transition()
-                        .duration(2500)
-                        .tween("rotate", function() {
-                            let r = d3.interpolate(projection.rotate(), [-p[0], -p[1]]);
-                            return t => {
-                                projection.rotate(r(t));
-                                svg.selectAll("path.land")
-                                    .attr("d", path)
-                                    .classed("focused", (d, i) => { 
-                                        return (d.id == focusedCountry.id) ? focused = d : false; 
-                                    });
-                            };
-                        })
-                    props.animateFauxDOM(2700)
-                });
-            props.drawFauxDOM()
-        }
+        return new Promise((resolve, reject) => {
+          this.setState({
+            falseDOM,
+            projection,
+            path,
+            svg,
+            countryToolTip,
+            countryList
+          },
+          resolve)
+        })
     }
+
+    readFiles() {
+      return Promise.all([
+          d3.json('/world-110m.json'),
+          d3.tsv('/world-110m-country-names.tsv')
+      ])
+    }
+
+    drawCountriesAndSetListeners(worldJson, countryData) {
+        let countries = topojson.feature(worldJson, worldJson.objects.countries).features
+
+        const countryById = {}
+
+        const {
+          countryList,
+          countryToolTip,
+          path,
+          svg
+        } = this.state
+
+        const props = this.props
+
+        // pushing countries into droplist
+        countryData.forEach(d => {
+            countryById[d.id] = d.name
+            countryList.append('option')
+                    .text(d.name)
+                    .property('value', d.id)
+        })
+
+        // drawing countries on globe
+        let world = svg.selectAll('path.land')
+            .data(countries)
+            .enter().append('path')
+            .attr('class', 'land')
+            .attr('d', path)
+
+        // hover on country info
+        world.on("mouseover", d => {
+            countryToolTip.text(countryById[d.id])
+            .style("left", (d3.event.pageX + 7) + "px")
+            .style("top", (d3.event.pageY - 15) + "px")
+            .style("display", "block")
+            .style("opacity", 1);
+            props.drawFauxDOM()
+          })
+          .on("mouseout", d => {
+            countryToolTip.style("opacity", 0)
+            .style("display", "none");
+            props.drawFauxDOM()
+          })
+          .on("mousemove", d => {
+            countryToolTip.style("left", (d3.event.pageX + 7) + "px")
+            .style("top", (d3.event.pageY - 15) + "px");
+            props.drawFauxDOM()
+          });
+
+        props.drawFauxDOM()
+
+        return new Promise((resolve, reject) => {
+          this.setState({
+            world,
+            countries,
+            countryData,
+            svg,
+            path
+          }, resolve)
+        })
+    }
+
+    attachGlobeSelectListener() {
+      let {
+        falseDOM,
+        countryData,
+        countries,
+        projection,
+        focused
+      } = this.state
+
+      const props = this.props
+
+      d3.select(falseDOM)
+      .select("select")
+      .on("change", e => {
+        const selectedName = d3.select(falseDOM)
+                              .select('.globe-select')
+                              .node().component.value
+
+        props.dispatch(selectCountryAction(selectedName))
+
+        const selectedId = countryData.find(e => e.name == selectedName).id
+
+        let rotate = projection.rotate(),
+            focusedCountry = countries.find(e => Number(e.id) === Number(selectedId)),
+            p = d3.geoCentroid(focusedCountry);
+
+        svg.selectAll(".focused").classed("focused", focused = false);
+
+        //Globe rotating
+        d3.transition()
+        .duration(2500)
+        .tween("rotate", () => {
+          let r = d3.interpolate(projection.rotate(), [-p[0], -p[1]]);
+          return t => {
+            projection.rotate(r(t));
+            svg.selectAll("path.land")
+              .attr("d", path)
+              .classed("focused", (d, i) => {
+                if (d.id == focusedCountry.id) {
+                  let focused = d
+                  this.setState({focused})
+                  return true
+                } else return false;
+              });
+          };
+        })
+        props.animateFauxDOM(2700)
+      });
+    }
+
 
     render() {
         return (
@@ -152,6 +221,6 @@ class GlobeSelector extends Component {
             </div>
         )
     }
-} 
+}
 
 export default connect()(withFauxDOM(GlobeSelector))
