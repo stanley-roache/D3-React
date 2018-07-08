bb06da1478ef4dc2e5132b1a525e26215ed4e5a0
stanley-roache <stan.roache@gmail.com>
2018-07-08 17:32:19 +1200

    removed log and unnecessary boiler file

--- client/reducers/data.js
+++ client/reducers/data.js
@@ -1,7 +1,6 @@
 export default function data(state = [], action) {
     switch (action.type) {
         case 'UPDATE_DATA':
-            console.log('updating data');
             return action.data
         default:
             return state
