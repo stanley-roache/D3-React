5c8380ffe74683814921d8e849f2b33301dd13bd
reubenjh <rjharcourt@gmail.com>
2018-07-09 08:46:31 +1200

    pop api

--- client/apis/population.js
+++ client/apis/population.js
@@ -2,17 +2,12 @@ import request from 'superagent'
 
 function getTotalByCountryFromXUntilY(country, startYear, endYear) {
   return request.get(`/population/total/${country}/${startYear}/${endYear}`)
-    .then(res => {
-      console.log('recieved data', res.body[0]);
-      return res.body
-    })
+    .then(res => res.body)
 }
 
 function getCountryList() {
   return request.get('/population/countries')
-    .then(res => {
-      return res.body
-    })
+    .then(res => res.body)
 }
 
 module.exports = {
