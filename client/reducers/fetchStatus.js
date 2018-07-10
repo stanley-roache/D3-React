6aa5bef500be80551ef0fcddf0f926092c5436b2
asalsfkcom <asd@alsfkh.com>
2018-07-10 16:25:00 +1200

    wrote status reducer

---
+++ client/reducers/fetchStatus.js
@@ -0,0 +1,36 @@
+export const initialState = {
+  countries: '',
+  data: ''
+}
+
+export default function data(state = [], action) {
+    switch (action.type) {
+        case 'REQUEST_COUNTRIES':
+            return {
+              data: state.data,
+              countries: 'fetching'
+            }
+        case 'RECEIVED_COUNTRIES':
+            return {
+              data: state.data,
+              countries: 'received'
+            }
+        case 'REQUEST_DATA':
+            return {
+              data: 'fetching',
+              countries: state.countries
+            }
+        case 'RECEIVED_DATA':
+            return {
+              data: 'received',
+              countries: state.countries
+            }
+        case 'CLOSE_GRAPH':
+          return {
+            data: '',
+            countries: state.countries
+          }
+        default:
+            return state
+    }
+}
--- client/reducers/index.js
+++ client/reducers/index.js
@@ -2,8 +2,10 @@ import {combineReducers} from 'redux'
 
 import country from './country'
 import data from './data'
+import fetchStatus from './fetchStatus'
 
 export default combineReducers({
   country,
-  data
+  data,
+  fetchStatus
 })
