851bb2c2405cfb9b37fc7fd6405e8acef3467ee2
catepalmer <catherinepalmer@gmail.com>
2018-08-01 15:33:17 +1200

    refactored and removed logs

--- server/routes/climateRoutes.js
+++ server/routes/climateRoutes.js
@@ -4,13 +4,10 @@ const router = require('express').Router()
 const climate = require('../apis/climate')
 
 router.get('/getData', (req, res) => {
-  console.log(`Router active, seeking data`);
-  const {start, end, country, metric, type} = req.query
+  const { start, end, country, metric, type } = req.query
+
   climate.getData(type, metric, start, end, country)
-    .then(data => {
-      console.log(`Success, returning data`);
-      res.json(data)
-    })
+    .then(data => { res.json(data) })
 })
 
 module.exports = router
--- server/routes/populationRoutes.js
+++ server/routes/populationRoutes.js
@@ -4,27 +4,17 @@ const router = require('express').Router()
 const pop = require('../apis/population')
 
 router.get('/total/:country/:start/:end/', (req, res) => {
-  console.log(`Population Router active, seeking data`);
-
-  const {country, start, end} = req.params
+  const { country, start, end } = req.params
 
   pop.getTotalByCountryFromXUntilY(country, start, end)
-    .then(data => {
-      console.log(`population router Success, returning`, data[0]);
-      res.json(data)
-    })
+    .then(data => { res.json(data) })
 })
 
 router.get('/detailed/:country/:start/:end/', (req, res) => {
-  console.log(`Population Router active, seeking data`);
-
-  const {country, start, end} = req.params
+  const { country, start, end } = req.params
 
   pop.getDetailedByCountryFromXUntilY(country, start, end)
-    .then(data => {
-      console.log(`population router Success, returning data`);
-      res.json(data)
-    })
+    .then(data => { res.json(data) })
 })
 
 router.get('/countries', (req, res) => {
