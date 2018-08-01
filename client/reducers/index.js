5301f8a5e718f18b27c3637c7c7c056702d2a852
catepalmer <catherinepalmer@gmail.com>
2018-08-01 15:32:53 +1200

    renamed data reducer update

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -1,13 +1,13 @@
 import {combineReducers} from 'redux'
 
 import country from './country'
-import data from './data'
+import popData from './popdata'
 import fetchStatus from './fetchStatus'
 import countryList from './countryList'
 
 export default combineReducers({
   country,
   countryList,
-  data,
+  popData,
   fetchStatus
 })
