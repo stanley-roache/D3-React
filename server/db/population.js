ba333075e9124842a3c4de7ef0ebf90e78f50958
Stanley Roache <stan.roache@gmail.com>
2018-08-08 17:54:40 +1200

    added db functions to retrieve, insert and update population data

--- server/db/population.js
+++ server/db/population.js
@@ -1,3 +1,41 @@
 const db = require('./connection')
 
+function getAvailableDataForCountry (name) {
+  return db('population')
+    .where({name})
+}
 
+function injectData (parcel) {
+  return checkExists(parcel.name)
+    .then(exists => {
+      return (exists)
+      ? updateCountryData(parcel)
+      : insertCountryData(parcel)
+    })
+}
+
+function updateCountryData (parcel) {
+  return db('population')
+    .where({name: parcel.name})
+    .update(parcel.years)
+}
+
+function insertCountryData (parcel) {
+  return db('population')
+    .insert({
+      name: parcel.name,
+      ...parcel.years
+    })
+}
+
+function checkExists (name) {
+  return db('population')
+    .where({name})
+    .first()
+    .then(result => !!result)
+}
+
+module.exports = {
+  getAvailableDataForCountry,
+  injectData
+}
