388414f4d21f11026079acbf5f79fe782eadec44
stanley-roache <stan.roache@gmail.com>
2018-08-14 13:33:56 +1200

    minor fix and comments to follow on

--- server/util/cachemanager.js
+++ server/util/cachemanager.js
@@ -2,7 +2,21 @@ const db = require('../db/population')
 const api = require('../apis/population')
 
 function fetchAndInjectAll(country, start, end) {
-  return pop.getTotalByCountryFromXUntilY(country, start, end)
+  return api.getTotalByCountryFromXUntilY(country, start, end)
+    .then(data => {
+        return db.injectData({
+          name: country,
+          years: data.map(item => ({
+            [item.year]: item.population
+          }))
+        }).then(() => data)
+    })
+}
+
+function fetchAndInjectMissing(data, start, end) {
+  // separate existing years from needed years, fetch, store
+
+  return api.getTotalByCountryFromXUntilY(country, start, end)
     .then(data => {
         return db.injectData({
           name: country,
