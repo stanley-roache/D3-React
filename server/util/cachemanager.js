a9a8b1c4733eabbf0bf056f5dba9df67244d5299
stanley-roache <stan.roache@gmail.com>
2018-08-14 13:14:57 +1200

    halfway through writing first cache function

--- server/util/cachemanager.js
+++ server/util/cachemanager.js
@@ -1,3 +1,17 @@
 const db = require('../db/population')
 const api = require('../apis/population')
 
+function fetchAndInjectAll (country, start, end) {
+  return pop.getTotalByCountryFromXUntilY(country, start, end)
+    .then(data => {
+      db.injectData({
+        name: country,
+        years: data
+      })
+    })
+}
+
+module.exports = {
+  fetchAndInjectAll,
+  fetchAndInjectMissing
+}
